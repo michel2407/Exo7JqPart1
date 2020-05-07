a4c560057faae5e8353c0e5d772cbf6e5d19eeb3
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-07 15:54:58 +0200

    Exercice 7 après correction avec la balise button

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+  // j'ajoute un élément click sur mon bouton (id bouton).
+  // on clique sur le bouton, j'affiche une alerte grâce à la fonction alert(), dans cette fonction on lui indique  ce qu'il doit afficher
+  $('#clickMe').click(function(){
+alert('bravo');
+  });
+});
